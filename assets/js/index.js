92c107c32c57a9133eedfde955e7d758ba4f4bda
zutrinken <xoxo@peteramende.de>
2015-02-28 00:44:22 +0100

    ground zero

---
+++ assets/js/index.js
@@ -0,0 +1,114 @@
+jQuery(function($) {
+
+	var body = $('body');
+	var viewport = $(window);
+
+	/* ==========================================================================
+	   Parallax cover
+	   ========================================================================== */
+	   
+	var cover = $('.cover');
+	var coverPosition = 0;
+
+	function prlx() {
+		if(cover.length >= 1) {
+			var windowPosition = viewport.scrollTop();
+			(windowPosition > 0) ? coverPosition = Math.floor(windowPosition * 0.25) : coverPosition = 0;
+			cover.css({
+				'transform' : 'translate(0, ' + coverPosition + 'px)'
+			});
+			(viewport.scrollTop() < cover.height()) ? body.addClass('cover-active') : body.removeClass('cover-active');
+		}
+	}
+	prlx();
+
+	viewport.on({
+		'scroll': function() {
+			prlx();
+		},
+		'resize': function() {
+			prlx();
+		},
+		'orientationchange': function() {
+			prlx();
+		}
+	});
+
+	/* ==========================================================================
+	   Reading Progress
+	   ========================================================================== */
+
+	var post = $('.post-content');
+	
+	function readingProgress() {
+		if(post.length >= 1) {
+			var postBottom = post.offset().top + post.height();
+			var windowBottom = viewport.scrollTop() + viewport.height();
+			var progress = 100 - (((postBottom - windowBottom) / (postBottom - viewport.height())) * 100);
+			$('.progress-bar').css('width', progress + '%');
+			(progress > 100) ? $('.progress-container').addClass('ready') : $('.progress-container').removeClass('ready');
+		}
+	}
+	readingProgress();
+
+	viewport.on({
+		'scroll': function() {
+			readingProgress();
+		},
+		'resize': function() {
+			readingProgress();
+		},
+		'orientationchange': function() {
+			readingProgress();
+		}
+	});
+
+	/* ==========================================================================
+	   Style code blocks with highlight and numbered lines
+	   ========================================================================== */
+
+	function codestyling() {
+		$('pre code').each(function(i, e) {
+			hljs.highlightBlock(e);
+			var code = $(this);
+			var lines = code.html().split(/\n/).length;
+			var numbers = [];
+			for (i = 1; i < lines; i++) {
+				numbers += '<span class="line">' + i + '</span>';
+			}
+			code.parent().append('<div class="lines">' + numbers + '</div>');
+		});
+	}
+	codestyling();
+
+	/* ==========================================================================
+	   Responsive Videos with Fitvids
+	   ========================================================================== */
+
+	function video() {
+		$('#wrapper').fitVids();
+	}
+	video();
+	
+	/* ==========================================================================
+	   Initialize and load Disqus
+	   ========================================================================== */
+
+	var disqusShortname = 'no-disqus'; //replace with your disqus shortname
+	
+	if (disqusShortname == 'no-disqus') {
+		$('.post-comments').css({
+			'display' : 'none'
+		});
+	} else {
+		$('#show-disqus').on('click', function() {
+			$.ajax({
+				type: "GET",
+				url: "http://" + disqusShortname + ".disqus.com/embed.js",
+				dataType: "script",
+				cache: true
+			});
+			$(this).parent().addClass('activated');
+		});
+	}
+});
