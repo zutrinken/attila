564066ff96dda653e9cec880ad2ee5d178e7a51b
zutrinken <xoxo@peteramende.de>
2015-08-16 00:04:44 +0200

    setup grunt 🐷

---
+++ Gruntfile.js
@@ -0,0 +1,94 @@
+module.exports = function(grunt) {
+    'use strict';
+    require('load-grunt-tasks')(grunt, {
+        pattern: ['grunt-*']
+    });
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        config: {
+            'cssSrcDir': 'src/sass',
+            'cssTargetDir': 'css',
+            'jsSrcDir': 'src/js',
+            'jsTargetDir': 'js'
+        },
+        copy: {
+	        dev: {
+                files: [{
+	                dest: 'assets/font/',
+	                src: '*',
+                    cwd: 'src/font/',
+                    expand: true
+                }]
+	        },
+	        dist: {
+                files: [{
+	                dest: 'assets/font/',
+	                src: '*',
+                    cwd: 'src/font/',
+                    expand: true
+                }]		        
+	        } 
+        },
+        clean: {
+            dev: ['dev'],
+            dist: ['dist'],
+            all: ['dev', 'dist']
+        },
+        sass: {
+            dev: {
+                options: {
+                    includePaths: ['<%= config.cssSrcDir %>'],
+                    sourceMaps: true
+                },
+                files: {
+                    'assets/<%=  config.cssTargetDir %>/style.css': '<%=  config.cssSrcDir %>/style.scss'
+                }
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'assets/<%=  config.cssTargetDir %>/style.css': '<%=  config.cssSrcDir %>/style.scss'
+                }
+            }
+        },
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('autoprefixer-core')({ browsers: ['last 2 versions'] })
+                ]
+            },
+            'dev': {
+                src: 'assets/<%=  config.cssTargetDir %>/*.css'
+            }
+        },
+		uglify: {
+			js: {
+				files: {
+					'assets/<%=  config.jsTargetDir %>/script.js': ['<%=  config.jsSrcDir %>/libs/jquery-*.js', '<%=  config.jsSrcDir %>/**/*.js']
+				}
+			}
+		},
+        watch: {
+            css: {
+                files: '<%=  config.cssSrcDir %>/**/*.scss',
+                tasks: ['sass:dev','copy:dev']
+            }
+        }
+    });
+
+    grunt.registerTask('build', [
+        'sass:dist',
+        'postcss',
+        'copy:dist',
+        'uglify'
+    ]);
+    grunt.registerTask('dev', [
+        'sass:dev',
+        'copy:dev',
+        'uglify'
+    ]);
+};
--- README.md
+++ README.md
@@ -24,6 +24,21 @@ A content focused responsive theme for [Ghost](http://github.com/tryghost/ghost/
 
 To enable [Disqus](https://disqus.com/) comments go to your blogs code injection settings and add `<script>var disqus = 'YOUR_DISQUS_SHORTNAME';</script>` to your blog header.
 
+## Development
+
+Install [Grunt](http://gruntjs.com/getting-started/):
+
+	npm install -g grunt-cli
+	
+Install Grunt dependencies:
+
+	npm install
+
+Build Grunt project:
+
+	grunt build
+	
+
 ## Copyright & License
 
 Copyright (C) 2015 Peter Amende - Released under the MIT License.
