6c3b7fff419e1350da55f91d8a4362dd40175e84
zutrinken <xoxo@zutrinken.com>
2018-09-03 11:42:49 +0200

    add gallery support. Fix max-width of wide iamges

--- src/js/script.js
+++ src/js/script.js
@@ -92,6 +92,22 @@ jQuery(function($) {
 	});
 
 	/* ==========================================================================
+	   Gallery
+	   ========================================================================== */
+
+	function gallery() {
+		var images = document.querySelectorAll('.kg-gallery-image img');
+		images.forEach(function (image) {
+			var container = image.closest('.kg-gallery-image');
+			var width = image.attributes.width.value;
+			var height = image.attributes.height.value;
+			var ratio = width / height;
+			container.style.flex = ratio + ' 1 0%';
+		});
+	}
+	gallery();
+
+	/* ==========================================================================
 	   Style code blocks with highlight and numbered lines
 	   ========================================================================== */
 
