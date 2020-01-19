5064811c1cbffbfb471b0694c767d55ef58187a0
zutrinken <xoxo@zutrinken.com>
2020-01-19 15:17:50 +0100

    change search behaviour

--- src/js/script.js
+++ src/js/script.js
@@ -46,26 +46,33 @@ jQuery(function($) {
   if (typeof ghosthunter_key === 'undefined') {
 
   } else {
+    var searchField = $("#search-field")
     $(".nav-search").css({
       'display': 'block'
     });
-    $('#search-field').ghostHunter({
-      results: "#results",
-      result_template: '<article class="post"><div class="inner"><div class="box post-box"><h2 class="post-title"><a href="{{link}}">{{title}}</a></h2><span class="post-meta">On <span class="post-date">{{pubDate}}</span></span></div></div></article>',
-      info_template: '<div class="blog-header"><div class="inner"><div class="archive archive-search box archive-box"><span class="archive-info"><span class="archive-type">Search</span><span class="archive-count">{{amount}} Posts</span></span><h1 class="archive-title">Results for "<span id="search-term"></span>"</h1></div></div></div>',
-      displaySearchInfo: true,
-      includebodysearch: true,
-      onComplete: function(results) {
-        if ($('#search-field').prop('value')) {
-          $('#search').show();
-          $('#default').hide();
-        } else {
-          $('#search').hide();
-          $('#default').show();
-        }
-        $('#search-term').text($('#search-field').prop('value'));
+
+    $('.nav-search').on({
+      'click': function() {
+        html.addClass('search-active');
+        searchField.focus();
         html.removeClass('menu-active');
       }
+    });
+
+    $('.search-close').on({
+      'click': function() {
+        html.removeClass('search-active');
+        searchField.val('');
+        $('#results').empty();
+      }
+    });
+
+    searchField.ghostHunter({
+      results: "#results",
+      result_template: '<article class="post"><div class="inner"><div class="box post-box"><h2 class="post-title"><a href="{{link}}">{{title}}</a></h2><span class="post-meta">On <span class="post-date">{{pubDate}}</span></span></div></div></article>',
+      info_template: '',
+      displaySearchInfo: true,
+      includebodysearch: true
     });
   }
 
