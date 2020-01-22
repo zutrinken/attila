f9c8ae2b5e2cb4da9374a1a781b3f0f08e37db5b
zutrinken <xoxo@zutrinken.com>
2020-01-22 13:48:08 +0100

    close search when key escape

--- src/js/script.js
+++ src/js/script.js
@@ -67,6 +67,16 @@ jQuery(function($) {
       }
     });
 
+    $(document).keydown(function(e) {
+      if (e.key === "Escape") { // escape key maps to keycode `27`
+        if (html.hasClass('search-active')) {
+          html.removeClass('search-active');
+          searchField.val('');
+          $('#results').empty();
+        }
+      }
+    });
+
     searchField.ghostHunter({
       results: "#results",
       result_template: '<article class="post"><div class="inner"><div class="box post-box"><h2 class="post-title"><a href="{{link}}">{{title}}</a></h2><span class="post-meta">On <span class="post-date">{{pubDate}}</span></span></div></div></article>',
