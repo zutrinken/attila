1fde6ff50322085f030f49089f9b17d4e1136280
Lewis Monteith <lewismonteith@gmail.com>
2020-04-25 16:15:39 +0100

    Mostly get the home page looking nice

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-console.log('hello');
+document
+  .querySelector('.hamburger')
+  .addEventListener('click', function(event) {
+    if (event.target.classList.contains('ri-menu-line')) {
+      event.target.classList.add('ri-close-line');
+      event.target.classList.remove('ri-menu-line');
+    } else {
+      event.target.classList.add('ri-menu-line');
+      event.target.classList.remove('ri-close-line');
+    }
+
+    document.querySelector('.header').classList.toggle('menu-open');
+  });
